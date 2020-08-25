d4bb51aa66df86cbd32173f31637895899c7d8aa
wxyzcctv <2846629226@qq.com>
2020-08-25 08:40:07 +0800

    搭建一个初步页面

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<div class="row">
+			<Cell />
+			<Cell />
+			<Cell />
+		</div>
+		<div class="row">
+			<Cell />
+			<Cell />
+			<Cell />
+		</div>
+		<div class="row">
+			<Cell />
+			<Cell />
+			<Cell />
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cell from "./components/Cell.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	components: {
+		Cell,
+	},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.row{
+  display: flex;
 }
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="cell" v-on:click="a = true">
+        <template v-if="a">{{text}}</template>
+        <template v-else></template>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{ a: false, text: "O"}
+    }
+}
+</script>
+
+<style>
+    .cell{
+        border: 1px solid black;
+        height: 100px;
+        width: 100px;
+        font-size: 80px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+
