79da42aaaa374015f12dae3c03522d11c54c2364
wxyzcctv <2846629226@qq.com>
2020-08-26 08:50:46 +0800

    简单实现井字游戏，不过有bug

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
 	<div>
+		<div>n的值是{{n}}</div>
 		<div class="row">
-			<Cell />
-			<Cell />
-			<Cell />
+			<Cell v-on:click="onClickCell(0,$event)" v-bind:n="n" />
+			<Cell v-on:click="onClickCell(1,$event)" v-bind:n="n" />
+			<Cell v-on:click="onClickCell(2,$event)" v-bind:n="n" />
 		</div>
 		<div class="row">
-			<Cell />
-			<Cell />
-			<Cell />
+			<Cell v-on:click="onClickCell(3,$event)" v-bind:n="n" />
+			<Cell v-on:click="onClickCell(4,$event)" v-bind:n="n" />
+			<Cell v-on:click="onClickCell(5,$event)" v-bind:n="n" />
 		</div>
 		<div class="row">
-			<Cell />
-			<Cell />
-			<Cell />
+			<Cell v-on:click="onClickCell(6,$event)" v-bind:n="n" />
+			<Cell v-on:click="onClickCell(7,$event)" v-bind:n="n" />
+			<Cell v-on:click="onClickCell(8,$event)" v-bind:n="n" />
 		</div>
+		<div>{{map}}</div>
+		<div>{{result}}</div>
 	</div>
 </template>
 
@@ -25,11 +28,66 @@ export default {
 	components: {
 		Cell,
 	},
+	data() {
+		return {
+			n: 0,
+			map: [
+				[null, null, null],
+				[null, null, null],
+				[null, null, null],
+			],
+			result: false,
+		};
+	},
+	methods: {
+		onClickCell(i, text) {
+			console.log(`我是第${i}号,我的内容是${text}`);
+			this.map[Math.floor(i / 3)][i % 3] = text;
+			this.n += 1;
+			this.tell();
+		},
+		tell() {
+			const map = this.map;
+			for (let i = 0; i < 2; i++) {
+				if (
+					map[i][0] !== null &&
+					map[i][0] === map[i][1] &&
+					map[i][1] === map[i][2]
+				) {
+					this.result = map[i][0];
+				}
+			}
+			for (let j = 0; j < 2; j++) {
+				if (
+					map[0][j] !== null &&
+					map[0][j] === map[1][j] &&
+					map[1][j] === map[2][j]
+				) {
+					this.result = map[0][j];
+				}
+			}
+			if (
+				map[0][0] !== null &&
+				map[0][0] === map[1][1] &&
+				map[1][1] === map[2][2]
+			) {
+				this.result = map[0][0];
+			}
+
+			if (
+				map[0][2] !== null &&
+				map[0][2] === map[1][1] &&
+				map[1][1] === map[2][0]
+			) {
+				this.result = map[0][2];
+			}
+		},
+	},
 };
 </script>
 
 <style>
-.row{
-  display: flex;
+.row {
+	display: flex;
 }
 </style>
--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,27 +1,41 @@
 <template>
-    <div class="cell" v-on:click="a = true">
-        <template v-if="a">{{text}}</template>
-        <template v-else></template>
-    </div>
+	<div>
+		{{n}}
+		<div class="cell" v-on:click="onClickSelf">
+			<template v-if="a">{{text}}</template>
+			<template v-else></template>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{ a: false, text: "O"}
-    }
-}
+	props: ["n"],
+	data() {
+		return { a: false, text: "" };
+	},
+	methods: {
+		onClickSelf() {
+			if (this.text !== "") {
+				return;
+			}
+			this.a = true;
+			this.text = this.n % 2 !== 0 ? "X" : "O";
+			this.$emit("click", this.text);
+		},
+	},
+};
 </script>
 
 <style>
-    .cell{
-        border: 1px solid black;
-        height: 100px;
-        width: 100px;
-        font-size: 80px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.cell {
+	border: 1px solid black;
+	height: 100px;
+	width: 100px;
+	font-size: 80px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 </style>
 
